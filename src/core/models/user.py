2a9a3207af59dfafa0c4167422af77d49eac5a10
Kowenjko <ortoswt@gmail.com>
2025-11-24 18:53:21 +0200

    fix model user and base

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,10 +1,10 @@
 from typing import TYPE_CHECKING
 
-from fastapi_users.db import (
+from fastapi_users_db_sqlalchemy import (
     SQLAlchemyBaseUserTable,
     SQLAlchemyUserDatabase,
 )
-
+from core.types.user_id import UserIdType
 from .base import Base
 from .mixins.id_int_pk import IdIntPkMixin
 
@@ -12,8 +12,8 @@ if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
+class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
-        return SQLAlchemyUserDatabase(session, User)
+        return SQLAlchemyUserDatabase(session, cls)
