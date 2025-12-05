17b2c6103845a64c0eb03b2bd2b1d9655c396ca6
Kowenjko <ortoswt@gmail.com>
2025-12-05 12:12:01 +0200

    add connect redis to fast api

--- src/create_fastapi_app.py
+++ src/create_fastapi_app.py
@@ -2,11 +2,17 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+
 from sqladmin import Admin
+
+from redis.asyncio import Redis
 
 from admin import register_admin_views
 
 from core.models import db_helper
+from core.config import settings
 from errors_handlers import register_errors_handlers
 from middlewares import register_middlewares
 
@@ -14,6 +20,15 @@ from middlewares import register_middlewares
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
+    redis = Redis(
+        host=settings.redis.host,
+        port=settings.redis.port,
+        db=settings.redis.db.cache,
+    )
+    FastAPICache.init(
+        RedisBackend(redis),
+        prefix=settings.cache.prefix,
+    )
     yield
     # shutdown
     await db_helper.dispose()
